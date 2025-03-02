412b4b96584bc95a7246545ab144f9b20b0914fd
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-02 10:11:32 +0630

    build HostVansDetail page

--- pages/host/HostVansDetail.jsx
+++ pages/host/HostVansDetail.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function HostVans() {
+  const {id} = useParams()
+  const [currentVan, setCurrentVan] = React.useState(null)
+  React.useEffect(()=> {
+    fetch(`/api/host/vans/${id}`)
+      .then((response) => response.json())
+      .then((data) => setCurrentVan(data.vans))
+  },[])
+
+  if(!currentVan) return <h2>Loading...</h2>
   return(
-    <div>
-      <h1>Host Vans Detail</h1>
-    </div>
+    <section>
+            <div className="host-van-detail-layout-container">
+                <div className="host-van-detail">
+                    <img src={currentVan.imageUrl} />
+                    <div className="host-van-detail-info-text">
+                        <i
+                            className={`van-type van-type-${currentVan.type}`}
+                        >
+                            {currentVan.type}
+                        </i>
+                        <h3>{currentVan.name}</h3>
+                        <h4>${currentVan.price}/day</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
   )
 }

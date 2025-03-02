1d5bc862a793dc00f44b764d3ee2edd9d838f1a9
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-02 22:28:34 +0630

    Filter the vans with a setter function

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -34,10 +34,30 @@ export default function Vans() {
     <div className="van-list-container">
       <h1>Explore our van options</h1>
       <div className="van-list-filter-buttons">
-        <Link className="van-type simple" to="?type=simple">Simple</Link>
-        <Link className="van-type luxury" to="?type=luxury">Luxury</Link>
-        <Link className="van-type rugged" to="?type=rugged">Rugged</Link>
-        <Link className="van-type clear-filters" to=".">Clear filter</Link>
+        <button
+          className="van-type simple"
+          onClick={() => setSearchParams({ type: "simple" })}
+        >
+          Simple
+        </button>
+        <button
+          className="van-type luxury"
+          onClick={() => setSearchParams({ type: "luxury" })}
+        >
+          Luxury
+        </button>
+        <button
+          className="van-type rugged"
+          onClick={() => setSearchParams({ type: "rugged" })}
+        >
+          Rugged
+        </button>
+        <button
+          className="van-type clear-filters"
+          onClick={() => setSearchParams({})}
+        >
+          Clear filter
+        </button>
       </div>
       <div className="van-list">{vanElements}</div>
     </div>

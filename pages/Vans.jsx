38f6784c0b970973fd12a288ca72ec1a6dc73515
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-02 22:19:05 +0630

    use search params

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function Vans() {
   const [vans, setVans] = React.useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const typeFilter = searchParams.get("type");
+
   React.useEffect(() => {
     fetch("/api/vans")
       .then((response) => response.json())
       .then((data) => setVans(data.vans));
   }, []);
+  const displayedVans = typeFilter
+    ? vans.filter((van) => van.type === typeFilter)
+    : vans;
 
-  const vanElements = vans.map((van) => (
+  const vanElements = displayedVans.map((van) => (
     <div className="van-tile" key={van.id}>
       <Link to={`/vans/${van.id}`}>
         <img src={van.imageUrl} />
@@ -27,6 +33,12 @@ export default function Vans() {
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
+      <div className="van-list-filter-buttons">
+        <Link className="van-type simple" to="?type=simple">Simple</Link>
+        <Link className="van-type luxury" to="?type=luxury">Luxury</Link>
+        <Link className="van-type rugged" to="?type=rugged">Rugged</Link>
+        <Link className="van-type clear-filters" to=".">Clear filter</Link>
+      </div>
       <div className="van-list">{vanElements}</div>
     </div>
   );
